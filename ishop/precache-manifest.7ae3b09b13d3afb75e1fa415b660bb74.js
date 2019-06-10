644cd1830b6314f1936df28d835f8f255b806d83
r1ruslan <codeez33@gmail.com>
2019-06-10 23:42:51 +0300

    first try of ishop launch

---
+++ ishop/precache-manifest.7ae3b09b13d3afb75e1fa415b660bb74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "babb25fbee8bad63bbef1c4261065fb2",
+    "url": "/ishop/index.html"
+  },
+  {
+    "revision": "f8e7dfff3d5d8e1f9096",
+    "url": "/ishop/static/css/2.4615749f.chunk.css"
+  },
+  {
+    "revision": "217ab138e78819de7e66",
+    "url": "/ishop/static/css/main.0c5c67db.chunk.css"
+  },
+  {
+    "revision": "f8e7dfff3d5d8e1f9096",
+    "url": "/ishop/static/js/2.132cf4d9.chunk.js"
+  },
+  {
+    "revision": "217ab138e78819de7e66",
+    "url": "/ishop/static/js/main.251de46f.chunk.js"
+  },
+  {
+    "revision": "5403c3002044bcae2950",
+    "url": "/ishop/static/js/runtime~main.03ba53f4.js"
+  },
+  {
+    "revision": "54d5cd8e4f1602391df2c81011d718b7",
+    "url": "/ishop/static/media/favs.54d5cd8e.png"
+  }
+]);
