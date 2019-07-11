ef30d9bbd3a8f17fff304f0c160db5ff936ad753
r1ruslan <codeez33@gmail.com>
2019-07-11 23:20:27 +0300

    v 0.1.0

---
+++ new-project/precache-manifest.65799c49f9b77401350b40a89439dd90.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28da44c8643cd26784b298cdcb819360",
+    "url": "/new-project/index.html"
+  },
+  {
+    "revision": "ae8adf2b301134d94cfd",
+    "url": "/new-project/static/css/2.d8fd75e1.chunk.css"
+  },
+  {
+    "revision": "4d0f63af2dd369c53cc4",
+    "url": "/new-project/static/css/main.233099db.chunk.css"
+  },
+  {
+    "revision": "ae8adf2b301134d94cfd",
+    "url": "/new-project/static/js/2.4ee7dd06.chunk.js"
+  },
+  {
+    "revision": "4d0f63af2dd369c53cc4",
+    "url": "/new-project/static/js/main.ba87009c.chunk.js"
+  },
+  {
+    "revision": "c0c3d5003c70a07487a5",
+    "url": "/new-project/static/js/runtime~main.4c28f756.js"
+  },
+  {
+    "revision": "b1f0a0d022cee20cfac100ec72a56623",
+    "url": "/new-project/static/media/apple-watch.b1f0a0d0.svg"
+  },
+  {
+    "revision": "2b0fabd6f12bd59a18f5e5b125920a46",
+    "url": "/new-project/static/media/cable.2b0fabd6.svg"
+  },
+  {
+    "revision": "96d77809c709bafe257a723fbc313ed7",
+    "url": "/new-project/static/media/cancel-small.96d77809.svg"
+  },
+  {
+    "revision": "7a4c4ed3e537d3fb59c7decd495bfec7",
+    "url": "/new-project/static/media/cancel.7a4c4ed3.svg"
+  },
+  {
+    "revision": "7383659795c68de83bffa23eeb597c55",
+    "url": "/new-project/static/media/filter-results-button.73836597.svg"
+  },
+  {
+    "revision": "41321034d1eb1e68aaea5ac8eec2b757",
+    "url": "/new-project/static/media/headphones.41321034.svg"
+  },
+  {
+    "revision": "1788ec3653421ad20663f03079fad49e",
+    "url": "/new-project/static/media/heart.1788ec36.svg"
+  },
+  {
+    "revision": "8cd7042e33bf1da77558bf25f92c7480",
+    "url": "/new-project/static/media/home.8cd7042e.svg"
+  },
+  {
+    "revision": "6e72882cf30c7f10ecbcd3817b81ac4f",
+    "url": "/new-project/static/media/iphone.6e72882c.svg"
+  },
+  {
+    "revision": "c28374a285074b6fce742e5868f91128",
+    "url": "/new-project/static/media/laptop.c28374a2.svg"
+  },
+  {
+    "revision": "df1a2837564c41c62d81e635a377d368",
+    "url": "/new-project/static/media/like.df1a2837.svg"
+  },
+  {
+    "revision": "0bfccc507904a5aa51f3cb6ab4bf6759",
+    "url": "/new-project/static/media/monitor.0bfccc50.svg"
+  },
+  {
+    "revision": "17f73bfcf766880dbb41f2102286b20a",
+    "url": "/new-project/static/media/pulse.17f73bfc.svg"
+  },
+  {
+    "revision": "fdcb51e4ca34b88215666fe26d301307",
+    "url": "/new-project/static/media/right-arrow.fdcb51e4.svg"
+  },
+  {
+    "revision": "2e54ebb5b46883c017e19527bccc535f",
+    "url": "/new-project/static/media/search.2e54ebb5.svg"
+  },
+  {
+    "revision": "3e94999488846da168efbcd9eb7ed8c7",
+    "url": "/new-project/static/media/shopping-cart.3e949994.svg"
+  },
+  {
+    "revision": "5524fc35552c821bb2412410674efede",
+    "url": "/new-project/static/media/verified.5524fc35.svg"
+  }
+]);
